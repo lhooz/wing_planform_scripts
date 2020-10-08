95c49a0b377e81cc06a8d40fc4bb183fe71d7c19
hao <hao.lee0019@gmail.com>
2020-10-08 13:19:52 +0100

    added save wing profile scripts

--- wing_planform_main.py
+++ wing_planform_main.py
@@ -1,8 +1,11 @@
 """main script for wing planform creation and parameter estimation"""
 
+import os
+import shutil
 from planform_functions import beta_wing_planform, radius_locations, force_estimate
+import numpy as np
 
-T_c = 0.04
+T_c = 0.05
 T = 3
 Re = 10000
 phi = 160
@@ -14,6 +17,14 @@ r1_hat = [0.4, 0.45, 0.5, 0.55, 0.6]
 
 x_off_set = [0.25]
 
+out_profile_folder = 'wing_profiles'
+#-------------------------------------------
+cwd = os.getcwd()
+out_profile_path = os.path.join(cwd, out_profile_folder)
+if os.path.exists(out_profile_path):
+    shutil.rmtree(out_profile_path)
+os.mkdir(out_profile_path)
+#-------------------------------------------
 with open('wplanform_data.csv', 'w') as f:
     f.write('%s%s\n' % (r't/c = ', str(T_c)))
     f.write('%s%s%s\n' % ('Thickness = ', str(T), ' mm'))
@@ -44,3 +55,16 @@ for ar in AR:
                 parameters_mm = [str(x) for x in parameters_mm]
                 with open('wplanform_data.csv', 'a') as f:
                     f.write('%s\n' % ', '.join(parameters_mm))
+
+                profile_name = 'ar' + str(ar) + '_ofs' + str(
+                    y_off) + '_r1h' + str(r1) + '.csv'
+                profile_file = os.path.join(out_profile_path, profile_name)
+                wing_profile = np.append(LE, np.flip(TE, axis=0),
+                                         axis=0) * 1000
+
+                with open(profile_file, 'w') as f:
+                    f.write('x(chord_dir), y(span_dir)\n')
+                for co in wing_profile:
+                    co_str = [str(x) for x in co]
+                    with open(profile_file, 'a') as f:
+                        f.write('%s\n' % ', '.join(co_str))
