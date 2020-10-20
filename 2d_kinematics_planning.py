676bf9f7fadd7c5908ea286b01a267a166242eb0
hao <hao.lee0019@gmail.com>
2020-10-21 00:23:17 +0100

    changed to time fraction specification

--- 2d_kinematics_planning.py
+++ 2d_kinematics_planning.py
@@ -2,59 +2,58 @@
 
 import numpy as np
 
-Re = [100]
+Re = [100, 1000, 10000]
 stroke_dist = [3, 5, 7, 9]
-acc_dist_fraction = [0.125, 0.25, 0.5]  #-include both acc and decc dist-
-pitch_dist_fraction = [0.125, 0.25, 0.5]
+acc_time_fraction = [0.125, 0.25, 0.5]  #-include both acc and decc dist-
+pitch_time_fraction = [0.125, 0.25, 0.5]
 #---------------------------------------
 wing_chord = 1
+stroke_time = 1
 pitch_angle = 90
-pitch_acc_fraction = 0.1
+pitch_acc_time_fraction = 0.25
 #--------------------------------------
 with open('2d_case_parameters.csv', 'w') as f:
-    f.write('\nwing_chord = %s (m)\n' % str(wing_chord))
+    f.write('wing_chord = %s (m)\n' % str(wing_chord))
+    f.write('stroke_time = %s (m)\n' % str(stroke_time))
     f.write('pitch_angle = %s (deg)\n' % str(pitch_angle))
-    f.write('pitch_acc_time_fraction = %s\n' % str(pitch_acc_fraction))
+    f.write('pitch_acc_time_fraction = %s\n' % str(pitch_acc_time_fraction))
     f.write(
         '%s\n' %
-        r'Re, stroke_dist, acc_dist_fraction, pitch_dist_fraction, acc_t (s), steady_t (s), pitch_t (s), nu (m^2/s), acc (m/s^2), pitch_acc (deg/s^2), steady_velocity (m/s), c_U (s)'
+        r'Re, stroke_dist, acc_time_fraction, pitch_time_fraction, acc_t (s), steady_t (s), pitch_t (s), nu (m^2/s), acc (m/s^2), pitch_acc (deg/s^2), steady_velocity (m/s), acc_dist (chords), steady_dist (chords), pitch_dist (chords), c_U (s)'
     )
 #--------------------------------------
 for Rei in Re:
     for strokei in stroke_dist:
-        for acc_df in acc_dist_fraction:
-            acc_dfi = acc_df / 2
-            for pitch_dfi in pitch_dist_fraction:
-                steady_velocity = 1
-                acc_t = acc_dfi * strokei * wing_chord / (0.5 *
-                                                          steady_velocity)
-                steady_t = (
-                    1 - 2 * acc_dfi) * strokei * wing_chord / steady_velocity
-                total_stroke_t = 2 * acc_t + steady_t
-
-                #---scaling to unit time--
-                acc_t = acc_t / total_stroke_t
-                steady_t = steady_t / total_stroke_t
-                steady_velocity = steady_velocity * total_stroke_t
+        for acc_tf in acc_time_fraction:
+            acc_t = acc_tf * stroke_time / 2
+            for pitch_tfi in pitch_time_fraction:
+                steady_t = stroke_time - 2 * acc_t
+                pitch_t = pitch_tfi * stroke_time
+                steady_velocity = strokei * wing_chord / (1 - acc_t)
                 #-------------------------
                 c_U = wing_chord / steady_velocity
-
                 acc = steady_velocity / acc_t
-                if pitch_dfi <= acc_dfi:
-                    pitch_t = (pitch_dfi * strokei * wing_chord /
-                               (0.5 * acc))**0.5
-                else:
-                    pitch_t = acc_t
-                    pitch_t += (pitch_dfi - acc_dfi
-                                ) * strokei * wing_chord / steady_velocity
-
                 pitch_avel = pitch_angle / (
-                    (1 - 0.5 * pitch_acc_fraction) * pitch_t)
-                pitch_acc = pitch_avel / (0.5 * pitch_acc_fraction * pitch_t)
+                    (1 - 0.5 * pitch_acc_time_fraction) * pitch_t)
+
+                #--travel distance calculation--
+                acc_dist = (steady_velocity / 2 * acc_t) / wing_chord
+                steady_dist = steady_velocity * steady_t / wing_chord
+                if pitch_t <= acc_t:
+                    pitch_dist = 0.5 * acc * pitch_t**2
+                else:
+                    pitch_dist = acc_dist
+                    pitch_dist += (pitch_t - acc_t) * steady_velocity
+                pitch_dist = pitch_dist / wing_chord
+                #-------------------------------
+
+                pitch_acc = pitch_avel / (0.5 * pitch_acc_time_fraction *
+                                          pitch_t)
                 nu = steady_velocity * wing_chord / Rei
                 parameters = [
-                    Rei, strokei, acc_df, pitch_dfi, acc_t, steady_t, pitch_t,
-                    nu, acc, pitch_acc, steady_velocity, c_U
+                    Rei, strokei, acc_tf, pitch_tfi, acc_t, steady_t, pitch_t,
+                    nu, acc, pitch_acc, steady_velocity, acc_dist, steady_dist,
+                    pitch_dist, c_U
                 ]
                 parameters = [str(x) for x in parameters]
 
